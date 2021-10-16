e24ad1db7d145a541e7d698756e304df019fa03a
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-10-16 11:01:02 +0200

    Switch to CommonJS for now
    Some build tools' config.ts files are compiled with the “module” option
    set inappropriately.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-import fs from 'fs';
-import { URL } from 'url';
-
-const pkg = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url).pathname, 'utf8'));
+const { URL } = require('url');
+const pkg = require('./package.json');
 
 const prefix = '@frida/';
 const shims = Object.entries(pkg.dependencies)
   .filter(([name, _]) => name.startsWith(prefix))
   .map(([name, _]) => name.substr(prefix.length));
 
-export default function all(rootUrl) {
+module.exports = function all(rootUrl) {
   return Object.fromEntries(shims.map(name => [name, new URL(`./node_modules/@frida/${name}`, rootUrl).pathname]));
 };
