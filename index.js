b31b02fa5e0fa5c1f1dba9d54ecbf4ca3f8903dc
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-05-06 23:23:42 +0200

    Tweak style

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const shims = Object.entries(pkg.dependencies)
   .map(([name, _]) => name.substr(prefix.length));
 
 module.exports = function all(rootUrl) {
-  const offset = process.platform === 'win32' ? 1 : 0;
+  const offset = (process.platform === 'win32') ? 1 : 0;
   return Object.fromEntries(shims.map(name => [name, new URL(`./node_modules/@frida/${name}`, rootUrl).pathname.substring(offset)]));
 };
